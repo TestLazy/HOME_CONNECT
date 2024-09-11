6deead834bfdbc0b581f6675a8d5a8eacbb5c50d
viegasync <brendlazy@gmail.com>
2024-09-11 03:39:43 -0300

    Initial Commit

--- src/main/java/com/home/connect/core/entities/UserEntity.java
+++ src/main/java/com/home/connect/core/entities/UserEntity.java
@@ -2,6 +2,7 @@ package com.home.connect.core.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,6 +15,7 @@ import java.util.function.Consumer;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
+@Table(name = "TB_USERS")
 public class UserEntity extends BaseEntity {
     @Column(unique = true, length = 15)
     private String email;
@@ -21,13 +23,13 @@ public class UserEntity extends BaseEntity {
     @Column(length = 60)
     private String password;
 
-    @Column(length = 60)
+    @Column(length = 60, name = "name")
     private String fullName;
 
     @Column(length = 1)
     private Integer permission;
 
-    @Column(unique = true, length = 15)
+    @Column(unique = true, length = 15, name = "cpf")
     private String personalNumber;
 
     public UserEntity(Consumer<UserEntity> entity) {entity.accept(this);}
