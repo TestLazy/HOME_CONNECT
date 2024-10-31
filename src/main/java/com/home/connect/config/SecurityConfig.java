e0722a7d2a78aac3cdbd1024904ba7b9fed820e1
Test <brendlazy@gmail.com>
2024-10-30 21:59:44 -0300

    CORS

--- src/main/java/com/home/connect/config/SecurityConfig.java
+++ src/main/java/com/home/connect/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(Customizer.withDefaults()))
 
+                .cors(AbstractHttpConfigurer::disable)
                 .build();
     }
 
